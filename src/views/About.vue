3dd1ee82f82b71df316b2651c2e8421cf8eaa15b
heiyanquan <2792652898@qq.com>
2020-02-06 23:25:04 +0800

    fix

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2 @click="toDemo">to demo page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      aboutData: 'aboutData'
+    }
+  },
+  methods: {
+    toDemo() {
+      this.$router.push('/demo')
+    }
+  },
+}
+</script>
---
+++ src/views/Demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demo">
+    <h1>This is an demo page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      demoData: 'demoData'
+    }
+  },
+}
+</script>
